1ca4ecda548d4400190c20a723d6384932030079
sunilpaulmathew <sunil.kde@gmail.com>
2022-03-25 18:27:28 +0100

    Improve credits page
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- app/src/main/java/com/nfs/nfsmanager/activities/CreditsActivity.java
+++ app/src/main/java/com/nfs/nfsmanager/activities/CreditsActivity.java
@@ -26,23 +26,10 @@ import java.util.ArrayList;
  */
 public class CreditsActivity extends AppCompatActivity {
 
-    private final ArrayList<SerializableItems> mData = new ArrayList<>();
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
-
-        mData.add(new SerializableItems("Kernel Adiutor", "Willi Ye", ContextCompat.getDrawable(this, R.drawable.ic_grarak), "https://github.com/Grarak"));
-        mData.add(new SerializableItems("libsu", "John Wu", ContextCompat.getDrawable(this, R.drawable.ic_topjohnwu),"https://github.com/topjohnwu"));
-        mData.add(new SerializableItems("German Translation", "simonk206", ContextCompat.getDrawable(this, R.drawable.ic_germany), null));
-        mData.add(new SerializableItems("Italian Translation", "andrea", ContextCompat.getDrawable(this, R.drawable.ic_italy),null));
-        mData.add(new SerializableItems("French Translation", "K1ks", ContextCompat.getDrawable(this, R.drawable.ic_france), null));
-        mData.add(new SerializableItems("Spanish Translation", "Peter A. Cuevas", ContextCompat.getDrawable(this, R.drawable.ic_spain),null));
-        mData.add(new SerializableItems("Portuguese (rPt) Translation", "BrauliX", ContextCompat.getDrawable(this, R.drawable.ic_portugal), "https://github.com/BrauliX"));
-        mData.add(new SerializableItems("Portuguese (rBr) Translation", "BrauliX", ContextCompat.getDrawable(this, R.drawable.ic_brazil),"https://github.com/BrauliX"));
-        mData.add(new SerializableItems("Russian Translation", "Ramazan Magomedov", ContextCompat.getDrawable(this, R.drawable.ic_russia),"https://github.com/RamazanMagomedov"));
-        mData.add(new SerializableItems("Indonesian Translation", "Hafitz Setya", ContextCompat.getDrawable(this, R.drawable.ic_indonesia),"https://github.com/breakdowns"));
 
         AppCompatImageButton mBack = findViewById(R.id.back);
         MaterialTextView mVersion = findViewById(R.id.version);
@@ -53,7 +40,7 @@ public class CreditsActivity extends AppCompatActivity {
         mBack.setOnClickListener(v -> onBackPressed());
 
         mRecyclerView.setLayoutManager(new GridLayoutManager(this, getSpanCount(this)));
-        CreditsAdapter mRecycleViewAdapter = new CreditsAdapter(mData);
+        CreditsAdapter mRecycleViewAdapter = new CreditsAdapter(getData());
         mRecyclerView.setAdapter(mRecycleViewAdapter);
         mRecyclerView.setVisibility(View.VISIBLE);
     }
@@ -61,6 +48,21 @@ public class CreditsActivity extends AppCompatActivity {
     private int getSpanCount(Activity activity) {
         return Utils.isTablet(activity) ? Utils.getOrientation(activity) == Configuration.ORIENTATION_LANDSCAPE ?
                 4 : 3 : Utils.getOrientation(activity) == Configuration.ORIENTATION_LANDSCAPE ? 3 : 2;
+    }
+
+    private ArrayList<SerializableItems> getData() {
+        ArrayList<SerializableItems> mData = new ArrayList<>();
+        mData.add(new SerializableItems("Kernel Adiutor", "Willi Ye", ContextCompat.getDrawable(this, R.drawable.ic_grarak), "https://github.com/Grarak"));
+        mData.add(new SerializableItems("libsu", "John Wu", ContextCompat.getDrawable(this, R.drawable.ic_topjohnwu),"https://github.com/topjohnwu"));
+        mData.add(new SerializableItems("German Translation", "simonk206", ContextCompat.getDrawable(this, R.drawable.ic_germany), null));
+        mData.add(new SerializableItems("Italian Translation", "andrea", ContextCompat.getDrawable(this, R.drawable.ic_italy),null));
+        mData.add(new SerializableItems("French Translation", "K1ks", ContextCompat.getDrawable(this, R.drawable.ic_france), null));
+        mData.add(new SerializableItems("Spanish Translation", "Peter A. Cuevas", ContextCompat.getDrawable(this, R.drawable.ic_spain),null));
+        mData.add(new SerializableItems("Portuguese (rPt) Translation", "BrauliX", ContextCompat.getDrawable(this, R.drawable.ic_portugal), "https://github.com/BrauliX"));
+        mData.add(new SerializableItems("Portuguese (rBr) Translation", "BrauliX", ContextCompat.getDrawable(this, R.drawable.ic_brazil),"https://github.com/BrauliX"));
+        mData.add(new SerializableItems("Russian Translation", "Ramazan Magomedov", ContextCompat.getDrawable(this, R.drawable.ic_russia),"https://github.com/RamazanMagomedov"));
+        mData.add(new SerializableItems("Indonesian Translation", "Hafitz Setya", ContextCompat.getDrawable(this, R.drawable.ic_indonesia),"https://github.com/breakdowns"));
+        return mData;
     }
 
 }
